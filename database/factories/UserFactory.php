eea4e477b2e85f715d2e1ddc44f7910d33816af3
shadotoro <132667369+shadotoro@users.noreply.github.com>
2023-08-10 16:38:26 +0200

    migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
+
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
