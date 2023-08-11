cc01860af7f36d4a468bfd239168fe71945b164a
shadotoro <132667369+shadotoro@users.noreply.github.com>
2023-08-11 10:18:32 +0200

    recipe.php

--- app/Models/recipe.php
+++ app/Models/recipe.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class recipe extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description', 
+        'ingredients',
+        'instructions',
+        'user_id'
+    ];
+
+    public function user()
+    {   //une recette appartient à un utilisateur
+        return $this->belongsTo(User::class); 
+    }
 }
+
