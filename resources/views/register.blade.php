63bda327a603d5878ac011108438156f6e56fe27
Marion.girard <marion.girard''@gmail.com>
2023-08-12 13:17:02 +0200

    login

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,9 @@
 
 @section('contenu')
     <form action="/register" method="post">
+        {{ csrf_field() }}
         <input type="email" name="email" placeholder="Email">
+        <input type="text" name="user" placeholder="Email">
         <input type="password" name="password" placeholder="Mot de passe">
         <input type="password" name="password_confirmation" placeholder="Mot de passe (confirmation)">
         <input type="submit" value="M'inscrire">
