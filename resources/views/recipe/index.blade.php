4205ffd641753fb55b568cfad7f7028b57b3a045
shadotoro <132667369+shadotoro@users.noreply.github.com>
2023-08-15 14:49:35 +0200

    miam

--- resources/views/recipe/index.blade.php
+++ resources/views/recipe/index.blade.php
@@ -3,7 +3,7 @@
 
 <h1>Liste des recettes</h1>
 
-
+@include('recipe.create')
 <?php $recipes = DB::table('recipe')->get(); ?>
 @foreach ($recipes as $recipe)
     {{ $recipe->title }} - {{ $recipe->description }}<br>
