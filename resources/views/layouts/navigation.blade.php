3555831e935663fa1178a0db0253edb942860d92
Marion.girard <marion.girard''@gmail.com>
2023-08-15 16:08:26 +0200

    m

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link >
+                    <x-nav-link :href="route('recipe')" :active="request()->routeIs('recipe')">
                         {{ __('Recette') }}
                     </x-nav-link>
                 </div>
