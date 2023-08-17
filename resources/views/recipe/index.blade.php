3904589a41ecb087fe58cb21e65b1a408c4270c3
Marion.girard <marion.girard''@gmail.com>
2023-08-17 16:20:53 +0200

    merge finit

--- resources/views/recipe/index.blade.php
+++ resources/views/recipe/index.blade.php
@@ -24,7 +24,7 @@
         @foreach ($recipes as $recipe) 
         <div class="col-md-4 mb-4  ">
             <div class="border-black p-3 center-content">
-                <h2 class="font-weight-bold"> <a href="route('recipe.get')" {{ $recipe->id }}> {{ $recipe->title }} </a> </h2>
+                <h2 class="font-weight-bold"> <a href="{{ route('recipe.get', $recipe->id) }}"> {{ $recipe->title }} </a> </h2>
                 <p>   {{ $recipe->description }} <a href="{{route('recipe.edit')}}"> Modifier</a>   <p>  
             </div>
         </div>        
